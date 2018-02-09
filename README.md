628241741b968824180dc43002faf1a931bab424
Przemysław Chmielewski <p.chmielewski91@o2.pl>
2018-02-09 14:46:01 +0100

    v0.1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Quiz
+
+The application was created in November 2017. It reads a .txt file with a list of questions, displays them on the screen, reads the player's answer and counts points.
+
+I used IDE:
+- CodeBlocks
---
+++ pytanie.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Pytanie
+{
+    public:
+
+    string tresc;
+    string a, b, c, d;
+    int nr_pytania;
+    string poprawna;
+    string odpowiedz;
+    int punkt; // 1 lub 0
+
+    void wczytaj(); // wczytuje pytania z pliku
+    void zadaj(); // pokazuje pytanie, czyta odpiwiedz
+    void sprawdz(); // sprawdza poprawnosc odpowiedzi
+
+
+};
